37eb37bb3ef7808ebe5bae05f958bb5bc6555856
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-09 17:10:50 +0530

    Add tests for reverse complement and total base calculation

--- tests/test_fasta_stats.py
+++ tests/test_fasta_stats.py
@@ -1,5 +1,3 @@
-#test reverse complement
-#test total bases
 import pytest
 from bio_seq_v1.stats import sequence
 from bio_seq_v1.fasta import fasta_parser
@@ -16,4 +14,17 @@ def test_fasta_parser():
 #test gc content     
 def test_gc_content():
     gc_content = parsed[0].gc_content()
-    assert round(gc_content, 2) == 53.33    
+    assert round(gc_content, 2) == 53.33  
+
+#test total bases
+def test_total_bases():
+    base_count = parsed[0].base_count()
+    assert base_count["A"] == 10
+    assert base_count["C"] == 9
+    assert base_count["G"] == 15 
+    assert base_count["T"] == 11
+
+#test reverse complement
+def test_reverse_compliment():
+    reverse_comp = parsed[0].rev_complement()
+    assert reverse_comp == "CTAGCTAGCTAGCTAGCCCCGATCGCTAACTAGCTACGTACGCAT"
