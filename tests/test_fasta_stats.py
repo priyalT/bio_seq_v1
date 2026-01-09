d449e8d9f9e99156e40cd9f3512f0c713cd6e287
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-09 16:59:37 +0530

    Add test for parser and gc content

--- tests/test_fasta_stats.py
+++ tests/test_fasta_stats.py
@@ -1,13 +1,19 @@
-#test fasta parser
-#test gc content
 #test reverse complement
 #test total bases
 import pytest
-from bio_seq_v1.stats import (print_sequence_lengths, base_count, gc_content, rev_complement)
-
+from bio_seq_v1.stats import sequence
+from bio_seq_v1.fasta import fasta_parser
 fasta_seq = "tests/data/tiny.fasta"
-
+parsed = fasta_parser(fasta_seq)
+#test fasta parser
 def test_fasta_parser():
-    result = print_sequence_lengths(fasta_seq)
-     
+    assert isinstance(parsed, list)
+    assert all(isinstance(s, sequence) for s in parsed)
+    first_seq = parsed[0]
+    assert first_seq.sequence == "ATGCGTACGTAGCTAGTTAGCGATCGGGGCTAGCTAGCTAGCTAG"
+    assert first_seq.id == "seq1"
 
+#test gc content     
+def test_gc_content():
+    gc_content = parsed[0].gc_content()
+    assert round(gc_content, 2) == 53.33    
