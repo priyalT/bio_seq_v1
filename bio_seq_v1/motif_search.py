e626a3283459165d1ab7173c31ba96cfacfafaa0
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-19 08:07:26 +0530

    Add character matching, iupac conversion to regex, and mismatch methods

--- bio_seq_v1/motif_search.py
+++ bio_seq_v1/motif_search.py
@@ -1,6 +1,6 @@
 class Match():
     def __init__(self, seq_id, position, matched_seq, strand_attributes):
-        self,seq_id = seq_id
+        self.seq_id = seq_id
         self.position = position
         self.matched_seq = matched_seq
         self.strand_attributes = strand_attributes
@@ -45,11 +45,44 @@ class MotifFinder():
             raise TypeError("All DNA sequences must be strings")
         self._validate_sequences()
 
+    def _convert_iupac_to_regex (self, motif: str):
+        motif = motif.upper()
+        regex_parts = []
+        for char in motif:
+            if char not in self.IUPAC:
+                raise ValueError(f"Invalid IUPAC code: {char}")
+            bases = self.IUPAC[char]
+            if len(bases) == 1:
+                regex_parts.append(next(iter(bases)))
+            else:
+                regex_parts.append(f"[{''.join(sorted(bases))}]")
+        return "".join(regex_parts)
+    
     def _validate_sequences(self):
         valid = set(self.IUPAC.keys())
         for seq in self.dna:
             if not set(seq).issubset(valid):
                 raise ValueError(f"Invalid character in sequence: {seq}")
+            
+    def char_match(self, motif_char: str, base: str) -> bool:
+        return base in self.IUPAC[motif_char]
+
+    def mismatches(self, motif: str, kmer: str) -> int:
+        if len(motif) != len(kmer):
+            raise ValueError("Motif and k-mer must be of the same length")
+        
+        mismatches = 0
+        for m,b in zip(motif, kmer):
+            if not self.char_match(m,b):
+                mismatches += 1
+        return mismatches
+    
+    def kmer_generation(self, seq :str) -> list[str]:
+        return [
+
+        ]
+    
+
 
         
 
