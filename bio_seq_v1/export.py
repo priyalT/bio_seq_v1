476c1b9ca1678f45dae322e7f3f96dcdef1140be
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-21 23:10:43 +0530

    Create export utilities module

--- bio_seq_v1/export.py
+++ bio_seq_v1/export.py
@@ -5,13 +5,20 @@ from bio_seq_v1.translator import Translator
 from bio_seq_v1.orf import ORFDetector, ORF
 from bio_seq_v1.motif_search import MotifFinder, Match
 
-class Exporter():
+class Exporter:
     def __init__(self, path):
         if path:
             self.path = Path(path) 
         else:
             self.path = Path(".")
     
-    def to_csv(self):
-
+    def to_csv(self, path_or_buf=None, *, 
+               sep=',', na_rep='', float_format=None, 
+               columns=None, header=True, index=True, 
+               index_label=None, mode='w', encoding=None, 
+               compression='infer', quoting=None, quotechar='"', 
+               lineterminator=None, chunksize=None, date_format=None, 
+               doublequote=True, escapechar=None, decimal='.', errors='strict', 
+               storage_options=None):
+        parser = FASTAParser()
     
