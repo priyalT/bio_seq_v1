b2124d2efb5b79e3ee4e2fbea6d64f34b3dced5d
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-01-26 08:27:48 +0530

    Fix errors

--- bio_seq_v1/orf.py
+++ bio_seq_v1/orf.py
@@ -1,13 +1,14 @@
 from bio_seq_v1.translator import Translator
 
 class ORF():
-    def __init__(self, start, end, frame, strand, protein):
+    def __init__(self, seq_id, start, end, frame, strand, protein):
         if start < 0 or end < start:
             raise ValueError("Invalid ORF coordinates")
         if frame not in (0, 1, 2):
             raise ValueError("Frame must be 0, 1 or 2")
         if strand not in ("+", "-"):
             raise ValueError ("Strand must be '+' or '-'")
+        self.seq_id = seq_id
         self.start = start
         self.end = end
         self.frame = frame
@@ -17,6 +18,7 @@ class ORF():
 
     def to_dict(self):
         return {
+            'seq_id': self.seq_id,
             'start': self.start,
             'end': self.end,
             'frame': self.frame,
@@ -28,6 +30,7 @@ class ORF():
         if not isinstance(other, ORF):
             return NotImplemented
         return (
+            self.seq_id == other.seq_id and
             self.start == other.start and
             self.end == other.end and
             self.frame == other.frame and
@@ -48,6 +51,8 @@ class ORFDetector():
     def find_orfs(self, seq):
         orfs = []
         frames = self.translator.translate_six_frames(seq)
+        seq_id = seq.id
+        seq_len = len(seq.sequence)
         for frame_label, protein in frames.items():
             strand = frame_label[0]
             frame = int(frame_label[1]) - 1
@@ -60,18 +65,26 @@ class ORFDetector():
                     while scan < len(protein) and protein[scan] != "*":
                         scan += 1
 
-                    if scan < len(protein):  # stop codon found
+                    if scan < len(protein):  
                         end_aa = scan - 1
                         dna_start = frame + start_aa * 3
                         dna_end = frame + end_aa * 3 + 2
                         prot_seq = protein[start_aa:scan]
+                        
+                        if strand == "-":
+                            rc_start = dna_start
+                            rc_end = dna_end
+                            dna_start = seq_len - rc_end - 1
+                            dna_end = seq_len - rc_start - 1
+
 
                         if len(prot_seq) * 3 >= self.min_length:
                             orfs.append(
-                                ORF(dna_start, dna_end, frame, strand, prot_seq)
+                                ORF(seq_id, dna_start, dna_end, frame, strand, prot_seq)
                             )
 
                 aa_index += 1
+
         return orfs
         
     def overlapping_orfs(self, orfs: list):
@@ -92,5 +105,3 @@ class ORFDetector():
                 if orf1.start <= orf2.end and orf2.start <= orf1.end:
                     overlap.append((orf1, orf2))
         return overlap
-    
-            
