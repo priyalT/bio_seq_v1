20dd9e7c87c836c06ff567c1af1cf41c0fa8b879
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-02-17 14:45:52 +0530

    Comment out for debugging

--- tests/fastaparser_stats_pytest_test.py
+++ tests/fastaparser_stats_pytest_test.py
@@ -1,81 +1,81 @@
-import pytest
-from bio_seq_v1.stats import sequence
-from bio_seq_v1.fasta import FASTAParser
-fasta_seq = "/Users/priyaltripathi/bio_seq_v1/tests/data/tiny.fasta"
-single_seq = "/Users/priyaltripathi/bio_seq_v1/tests/data/single.fasta"
+# import pytest
+# from bio_seq_v1.stats import sequence
+# from bio_seq_v1.fasta import FASTAParser
+# fasta_seq = "/Users/priyaltripathi/bio_seq_v1/tests/data/tiny.fasta"
+# single_seq = "/Users/priyaltripathi/bio_seq_v1/tests/data/single.fasta"
 
-def test_fasta_parser():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    sequences = parser.sequences
-    assert isinstance(sequences, list)
-    assert all(isinstance(s, sequence) for s in sequences)  
-    first_seq = sequences[0].sequence  
-    assert first_seq == "ATGCGTACGTAGCTAGTTAGCGATCGGGGCTAGCTAGCTAGCTAG"
+# def test_fasta_parser():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     sequences = parser.sequences
+#     assert isinstance(sequences, list)
+#     assert all(isinstance(s, sequence) for s in sequences)  
+#     first_seq = sequences[0].sequence  
+#     assert first_seq == "ATGCGTACGTAGCTAGTTAGCGATCGGGGCTAGCTAGCTAGCTAG"
 
-#test length of sequence
-def test_seq_length():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    seq_obj = parser.sequences[0]
-    assert seq_obj.sequence_length() == 45
+# #test length of sequence
+# def test_seq_length():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     seq_obj = parser.sequences[0]
+#     assert seq_obj.sequence_length() == 45
     
 
-#test gc content     
-def test_gc_content():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    seq_obj = parser.sequences[0]
-    gc_content = seq_obj.gc_content()
-    assert round(gc_content, 2) == 53.33  
+# #test gc content     
+# def test_gc_content():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     seq_obj = parser.sequences[0]
+#     gc_content = seq_obj.gc_content()
+#     assert round(gc_content, 2) == 53.33  
 
-#test total bases
-def test_total_bases():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    seq_obj = parser.sequences[0]
-    base_count = seq_obj.base_count()
-    assert base_count["A"] == 10
-    assert base_count["C"] == 9
-    assert base_count["G"] == 15 
-    assert base_count["T"] == 11
+# #test total bases
+# def test_total_bases():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     seq_obj = parser.sequences[0]
+#     base_count = seq_obj.base_count()
+#     assert base_count["A"] == 10
+#     assert base_count["C"] == 9
+#     assert base_count["G"] == 15 
+#     assert base_count["T"] == 11
 
-#test reverse complement
-def test_reverse_complement():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    seq_obj = parser.sequences[0]
-    reverse_comp = seq_obj.rev_complement()
-    assert reverse_comp == "CTAGCTAGCTAGCTAGCCCCGATCGCTAACTAGCTACGTACGCAT"
+# #test reverse complement
+# def test_reverse_complement():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     seq_obj = parser.sequences[0]
+#     reverse_comp = seq_obj.rev_complement()
+#     assert reverse_comp == "CTAGCTAGCTAGCTAGCCCCGATCGCTAACTAGCTACGTACGCAT"
 
-#testing edge cases
-#test lowercase input
-def test_lowercase_input():
-    seq = sequence("id", "ctga")
-    assert seq.rev_complement() == "TCAG"
+# #testing edge cases
+# #test lowercase input
+# def test_lowercase_input():
+#     seq = sequence("id", "ctga")
+#     assert seq.rev_complement() == "TCAG"
 
-def test_single_base():
-    seq = sequence("id", "G")
-    assert seq.gc_content() == 100.0
+# def test_single_base():
+#     seq = sequence("id", "G")
+#     assert seq.gc_content() == 100.0
 
-def test_multiple_fasta_entries():
-    parser = FASTAParser(fasta_seq)
-    parser.parse_file()
-    seq = parser.sequences
-    assert len(seq) == 3
+# def test_multiple_fasta_entries():
+#     parser = FASTAParser(fasta_seq)
+#     parser.parse_file()
+#     seq = parser.sequences
+#     assert len(seq) == 3
 
-def test_single_fasta_entries():
-    parser = FASTAParser(single_seq)
-    parser.parse_file()
-    seq = parser.sequences
-    assert len(seq) == 1
+# def test_single_fasta_entries():
+#     parser = FASTAParser(single_seq)
+#     parser.parse_file()
+#     seq = parser.sequences
+#     assert len(seq) == 1
 
-def test_ambig_codes():
-    parser = FASTAParser(single_seq)
-    parser.parse_file()
-    seq_obj = parser.sequences[0]
-    assert seq_obj.rev_complement() == "KDMAAATTTCCCGGGBYACCCGGGTTTAAACCC"
+# def test_ambig_codes():
+#     parser = FASTAParser(single_seq)
+#     parser.parse_file()
+#     seq_obj = parser.sequences[0]
+#     assert seq_obj.rev_complement() == "KDMAAATTTCCCGGGBYACCCGGGTTTAAACCC"
 
-def test_empty_sequence():
-    seq = sequence("id", "ATATAT")
-    assert seq.gc_content() == 0.0
+# def test_empty_sequence():
+#     seq = sequence("id", "ATATAT")
+#     assert seq.gc_content() == 0.0
