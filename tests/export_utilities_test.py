883a1960558dd75fce840ccba61bfb902dc886ce
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-02-20 09:33:36 +0530

    Write property tests for output file format correctness and existence

--- tests/export_utilities_test.py
+++ tests/export_utilities_test.py
@@ -5,6 +5,7 @@ import json
 import sys
 import os
 import tempfile
+from bio_seq_v1.stats import sequence
 from bio_seq_v1.export import Exporter
 from hypothesis import given, strategies as st
 from bio_seq_v1.stats import sequence
@@ -12,27 +13,20 @@ from bio_seq_v1.translator import Translator
 from bio_seq_v1.orf import ORFDetector
 from bio_seq_v1.fasta import FASTAParser
 
-# #roundtrip = csv se export kia fir import karne pe data remains the same
-
 @given(st.text(alphabet="ACGTNRUYKMSWBDHV", min_size= 1))
-def test_csv_export_roundtrip(fasta):
-    export = Exporter()
+def test_output_file_writing(fasta):
     seq = sequence("id", fasta)
-    length = seq.sequence_length()
-    counts = seq.base_count
-
     with tempfile.NamedTemporaryFile(mode = 'w', delete = False, suffix='.csv') as tmp_file:
         tmp_filepath = tmp_file.name
-    
     try:
-        export.to_csv(length, tmp_filepath)
-    
+        Exporter.sequences_to_csv([seq], tmp_filepath)
         with open(tmp_filepath, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             loaded_data = list(reader)
+        
+        assert loaded_data[0]["id"] == seq.id
+        assert loaded_data[0]["sequence"] == seq.sequence
 
-        assert loaded_data == seq
-    
+        
     finally:
         os.unlink(tmp_filepath)
-
