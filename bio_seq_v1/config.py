e6fc7bef8b03e950894a02b13cf551733e3096bc
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-02-26 10:23:19 +0530

    Create config class and functions

--- bio_seq_v1/config.py
+++ bio_seq_v1/config.py
@@ -13,7 +13,64 @@ class Config:
             pass
 
     def load_config():
-        
+        config = {
+            'parsing': {
+                'strict_mode' : False,
+                'strict_file' : False,
+                'strict_sequence' : False,
+                'max_errors' : 100,
+                'case_sensitive' : False,
+                'allowed_characters' : 'ACGTUNRYSWKMBDHVacgtunryswkmbdhv-.'
+            },
+            'output' : {
+                    'decimal_places': 2,
+                    'cli_table_format' : 'grid',
+                    'export_formats' : {
+                        'markdown' : 'pipe'
+                    },
+                    'use_rich' : True,
+                    'color_scheme' : 'auto',
+                    'html_report' : {
+                        'template' : 'default',
+                        'interactive' : True,
+                        'include_plots' : True
+                    },
+                    'verbose' : False,
+                    'line_width' : 80,
+                    'show_line_numbers' : False,
+                    'uppercase_output' : True
+            },
+            'export' : {
+                'default_format' : 'csv',
+                'default_delimiter' : ',',
+                'include_headers' : True,           
+                'pretty_json' : True,               
+                'json_indent': 2                  
+            },
+            'statistics': {
+                'gc_include_ambigcodes' : False,
+                'count_gaps' : True,              
+                'count_unknown': True,
+                'preserve_case': False            
+            },
+            'translation' : {
+                'genetic_code' : 'standard',
+                'start_codons': ['ATG'],
+                'stop_codons': ['TAA', 'TAG', 'TGA'],
+                'unknown_codon_char': 'X',
+                'include_stop_in_protein': False
+            },
+            'orf' : {
+                'min_length' : 0,                   
+                'min_protein_length' : 0,           
+                'search_both_strands': True,       
+                'allow_alternative_starts': False, 
+                'nested_orfs': True               
+            }
+            }
+                  
+
+        }
     
     def create_config():
         pass
