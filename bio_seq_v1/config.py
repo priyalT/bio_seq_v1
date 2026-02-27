ca9acb516e51b8260d42c1ba5f1b288b5322d9dc
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-02-27 14:27:29 +0530

    Edit config module

--- bio_seq_v1/config.py
+++ bio_seq_v1/config.py
@@ -14,9 +14,144 @@ class Config:
                 self._config_source = str(config_path)
 
         else:
-                print("No configuration found, using defaults.")
-        self._validate_config()  
+                self.create_config(config_path)
+        self._validate_config()
 
+    def create_config(self):
+        user_in = input("Since no config files were found, the following options will allow you to set some general defaults according to your liking." \
+              "Would you like to input these defaults? If not, the config file formed will contain defaults set beforehand. (Y/N)"
+        )
+        if user_in == "Y":
+            defaults = self._get_defaults
+
+            strict_mode = input("Strict mode on (T/F): ")
+            if strict_mode != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            strict_file = input("Strict file parsing (T/F): ")
+            if strict_file != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            strict_sequence = input("Strict sequence parsing (T/F): ")
+            if strict_sequence != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            case_sensitive = input("Case sensitive parsing (T/F): ")
+            if case_sensitive != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            
+            default_format = input("Default format for export (csv, tsv or json): ")
+            if default_format != "csv" or "tsv" or "json":
+                raise ValueError("Value other than csv, tsv or json entered. You can export currently in csv, tsv or json only.")
+            include_headers = input("Include headers in export? (T/F) ")
+            if include_headers != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            
+            gc_include_ambigcodes = input("Include ambiguous codes during sequence statistics? (T/F) ")
+            if gc_include_ambigcodes != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            count_unknown = input("Count unknown characters during sequence statistics? (T/F) ")
+            if count_unknown != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            
+            start_codons_input = input("Enter start codons separated by commas (e.g., ATG, GTG, TTG): ")
+            start_codons = [codon.strip().upper() 
+                            for codon in start_codons_input.split(",")    
+                            if codon.strip()]
+            stop_codons_input = input("Enter stop codons separated by commas (e.g., TAA, TAG, TGA): ")
+            stop_codons = [codon.strip().upper() 
+                            for codon in stop_codons_input.split(",")    
+                            if codon.strip()]
+            unknown_codon_char = str(input("Please enter the character used to deal with unknown codons (e.g., X): "))
+
+            search_both_strands = input("Allow orf search on both strands? (T/F) ")
+            if search_both_strands != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            allow_alternative_starts = input("Allow orf search with alternate starts? (T/F) ")
+            if allow_alternative_starts != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            nested_orfs = input("Allow nested orf search? (T/F) ")
+            if nested_orfs != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            
+            search_reverse_strand = input("Allow search on reverse strand for motif? (T/F) ")
+            if search_reverse_strand != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            iupac_support = input("Allow IUPAC support? (T/F) ")
+            if iupac_support != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+            
+            logging = input("Enable logging? (T/F) ")
+            if logging != "T" or "F":
+                raise ValueError("Value other than T or F entered. Please enter only T or F.")
+
+
+            user_inputs = {
+                'parsing': {
+                    'strict_mode' : strict_mode,
+                    'strict_file' : strict_file,
+                    'strict_sequence' : strict_sequence,
+                    'case_sensitive' : case_sensitive,
+            },
+                'export' : {
+                    'default_format' : default_format,
+                    'include_headers' : include_headers,
+            },
+                'statistics': {
+                    'gc_include_ambigcodes' : gc_include_ambigcodes,
+                    'count_unknown': count_unknown,
+            },
+                'translation' : {
+                    'start_codons': start_codons,
+                    'stop_codons': stop_codons,
+                    'unknown_codon_char': unknown_codon_char,
+            },
+                'orf' : {
+                    'search_both_strands': search_both_strands,
+                    'allow_alternative_starts': allow_alternative_starts,
+                    'nested_orfs': nested_orfs
+            },
+                'motif' : {
+                    'search_reverse_strand': search_reverse_strand,
+                    'iupac_support': iupac_support
+            },
+                'logging': {
+                    'enabled': logging,
+            }}
+
+            for inp in user_inputs.values():
+                if isinstance(inp, dict):
+                    for x in inp.values():
+                        if x == "T":
+                            x = True
+                        if x == "F":
+                            x = False
+                else: 
+                    for x in inp.values():
+                        if x == "T":
+                            x = True
+                        if x == "F":
+                            x = False
+
+                    
+                             
+                 
+        
+        elif user_in == "N":
+            if output_path is None:
+                output_path = Path.home()/ '.bio_seq' / 'config.yaml'
+            output_path = Path(output_path)
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+            with open (output_path, 'w') as f:
+                yaml.dump(self._get_defaults(), f, default_flow_style=False, sort_keys=False)
+            print(f"Config file created at: {output_path} using general defaults.")
+            
+        else:
+             raise ValueError("Value other than Y or N passed. Please enter either Y or N.")
+
+         # and then merges user_input w get_defaults
+         # and then creates config of this new dictionary that has been created
+         
+
+
+    
     def _get_defaults(self):
         return {
             'parsing': {
@@ -139,14 +274,6 @@ class Config:
              print("Using default configuration.")
 
     
-    def create_config(self, output_path = None):
-        if output_path is None:
-            output_path = Path.home()/ '.bio_seq' / 'config.yaml'
-        output_path = Path(output_path)
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        with open (output_path, 'w') as f:
-            yaml.dump(self._get_defaults(), f, default_flow_style=False, sort_keys=False)
-        print(f"Config file created at: {output_path}")
 
     def get_config(self, key_path, default=None):
          keys = key_path.split('.')
@@ -185,6 +312,5 @@ class Config:
         if self.config['motif']['default_k'] < 0:
              print("Warning: default_k cannot be negative, using 0")
              self.config['motif']['default_k'] = 0
-
 
              
